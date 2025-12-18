6718152ddf663382803ddd304d7de46c60724387
BruttherJOE <bruttherjoe@gmail.com>
2025-12-18 15:02:15 +0800

    Add files via upload

--- setup.bash
+++ setup.bash
@@ -9,6 +9,7 @@ sudo apt install -y wget gpg apt-transport-https software-properties-common
 sudo apt install -y terminator
 
 sudo apt install -y build-essential libfftw3-dev libasound2-dev libpulse-dev libtool automake libiniparser-dev libsdl2-2.0-0 libsdl2-dev libpipewire-0.3-dev libjack-jackd2-dev pkgconf
+sudo apt install -y cava
 
 sudo apt install -y nautilus
 
